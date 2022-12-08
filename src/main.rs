01ad7d02b7a29974dec4b045559d349c8b26d3df
Elliott Slaughter <slaughter@cs.stanford.edu>
2022-12-08 09:57:31 -0800

    Draw a bunch of rects.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,64 @@
 use macroquad::prelude::*;
 
+struct Rect {
+    x: f32,
+    y: f32,
+    w: f32,
+    h: f32,
+    vx : f32,
+    vy : f32,
+    color: Color,
+}
+
 #[macroquad::main("BasicShapes")]
 async fn main() {
+    let mut rects: Vec<Rect> = Vec::new();
+
+    const N: i32 = 1000;
+
+    for i in 0..N {
+        let w = rand::gen_range(0, screen_width() as i64 / 4);
+        let h = rand::gen_range(0, screen_height() as i64 / 4);
+        rects.push(Rect {
+            x: rand::gen_range(0, screen_width() as i64 - w) as f32,
+            y: rand::gen_range(0, screen_height() as i64 - h) as f32,
+            w: w as f32,
+            h: h as f32,
+            vx: rand::gen_range(-1.0, 1.0),
+            vy: rand::gen_range(-1.0, 1.0),
+            color: Color::from_rgba(
+                rand::gen_range(0, 255),
+                rand::gen_range(0, 255),
+                rand::gen_range(0, 255),
+                rand::gen_range(0, 255),
+            ),
+        });
+    }
+
+    let x: i32 = rand::gen_range(0, 4);
+
     loop {
-        clear_background(RED);
+        clear_background(WHITE);
 
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
+        for r in &mut rects {
+            draw_rectangle(r.x, r.y, r.w, r.h, r.color);
+            r.x += r.vx;
+            r.y += r.vy;
+            if r.x < 0.0 {
+                r.vx = -r.vx;
+            }
+            if r.y < 0.0 {
+                r.vy = -r.vy;
+            }
+            if r.x + r.w > screen_width() {
+                r.vx = -r.vx;
+            }
+            if r.y + r.h > screen_height() {
+                r.vy = -r.vy;
+            }
+        }
 
-        draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
+        draw_text("FPS: ", 20.0, 20.0, 30.0, DARKGRAY);
 
         next_frame().await
     }
